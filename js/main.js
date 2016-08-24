1bc0ab61177fa75bff7ebd16c23801d36ad9e9aa
Mata <mata.tf@gmail.com>
2016-08-24 22:53:31 +1200

    Trying to add age analysis

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var file;
 function getValue() {
     var userAccount = document.getElementById("userInput");
     function sendRedditRequest(file, callback) {
@@ -9,31 +10,66 @@ function getValue() {
         })
             .done(function (data) {
             var length = data.data.children.length;
-            for (var i = 0; i < length; i++) {
-                console.log(data.data.children[i].data.body);
-            }
-            //var comments = data.data.children[0].data.body.val;
+            //for(var i =0; i<length; i++){
+            //console.log(data.data.children[i].data.body);
+            //var comments = data.data.children[i].data.body.val;
+            //analyze(comments);
+            //}
+            callback(data);
             //analyze(comments);
         });
     }
-    sendRedditRequest();
+    sendRedditRequest(file, function (data) {
+        //var comments = data.data.children[1].data.body;
+        //console.log(comments);
+        //analyze();
+        var length = data.data.children.length;
+        for (var i = 0; i < length; i++) {
+            var temp = data.data.children[i].data.body;
+            var results = results + temp;
+        }
+        analysis(results);
+        //console.log(results);
+    });
     //console.log(comments);
 }
 ;
-function analyze(text) {
+function analysis(param) {
+    function sendUclassifyRequest(file, callback) {
+        $.ajax({
+            url: "https://api.uclassify.com/v1/uclassify/ageanalyzer/Classify?readkey=FWCr4N9FAiiD&text=" + param,
+            type: "GET",
+            data: file,
+            processData: false
+        })
+            .done(function (data) {
+            callback(data);
+        });
+    }
+    sendUclassifyRequest(file, function (scores) {
+        var res = scores["65-100"];
+        console.log(res);
+    });
+}
+/*function analyze(){
     var form = new FormData();
-    form.append("txt", text);
-    function requestAnalysis() {
+    form.append("txt", "Hello there mate");
+
+    function requestAnalysis(){
         $.ajax({
             url: "http://sentiment.vivekn.com/api/text/",
-            type: "POST",
+            method: "POST",
             mimeType: "multipart/form-data",
             data: form,
             processData: false
         })
-            .done(function (data) {
-            console.log("Done");
-            console.log(data.result.sentiment);
-        });
+        .done(function (data) {
+                console.log("Done");
+                console.log(data.result.sentiment);
+                
+
+            })
     }
+    requestAnalysis();
 }
+analyze();*/ 
--- js/main.ts
+++ js/main.ts
@@ -1,4 +1,4 @@
-
+var file;
 function getValue(){
     var userAccount : HTMLInputElement = <HTMLInputElement> document.getElementById("userInput");
 
@@ -12,29 +12,65 @@ function getValue(){
         .done(function (data) {
                 var length = data.data.children.length;
 
-                for(var i =0; i<length; i++){
-                console.log(data.data.children[i].data.body);
+                //for(var i =0; i<length; i++){
+                //console.log(data.data.children[i].data.body);
                 //var comments = data.data.children[i].data.body.val;
                 //analyze(comments);
-                }
-                //var comments = data.data.children[0].data.body.val;
+                //}
+                callback(data);
                 //analyze(comments);
 
             })
     }
 
-    sendRedditRequest();
+    sendRedditRequest(file,function(data){
+        //var comments = data.data.children[1].data.body;
+        //console.log(comments);
+        //analyze();
+
+        var length = data.data.children.length;
+
+        for(var i =0; i<length; i++){
+                var temp: string = data.data.children[i].data.body;
+                var results: string = results + temp;
+                }
+                
+                analysis(results);
+                //console.log(results);
+
+    });
     //console.log(comments);
 };
 
-function analyze(text){
+function analysis(param){
+
+    function sendUclassifyRequest(file, callback) : void {
+        $.ajax({
+            url: "https://api.uclassify.com/v1/uclassify/ageanalyzer/Classify?readkey=FWCr4N9FAiiD&text=" + param,
+            type: "GET",
+            data: file,
+            processData: false
+        })
+        .done(function (data) {
+                callback(data);
+            })
+    }
+    sendUclassifyRequest(file, function(scores){
+        var res = scores["65-100"];
+        console.log(res);
+    });
+
+}
+
+
+/*function analyze(){
     var form = new FormData();
-    form.append("txt", text);
+    form.append("txt", "Hello there mate");
 
     function requestAnalysis(){
         $.ajax({
             url: "http://sentiment.vivekn.com/api/text/",
-            type: "POST",
+            method: "POST",
             mimeType: "multipart/form-data",
             data: form,
             processData: false
@@ -46,4 +82,6 @@ function analyze(text){
 
             })
     }
+    requestAnalysis();
 }
+analyze();*/
